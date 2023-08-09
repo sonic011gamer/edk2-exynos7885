fb60a6fdf0d490f02e07b40ad43b6533b846ebca
sonic011gamer <efimgirjeu@gmail.com>
2023-08-09 07:12:50 +0300

    Add workspace/ directory and use all cores for building

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
 ./build_bootshim.sh
-GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p EXYNOS7885Pkg/Devices/a10.dsc
+GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -j$(nproc) -s -n 0 -a AARCH64 -t GCC5 -p EXYNOS7885Pkg/Devices/a10.dsc
 cat BootShim/BootShim.bin workspace/Build/EXYNOS7885Pkg/DEBUG_GCC5/FV/EXYNOS7885PKG_UEFI.fd > workspace/UEFI
 mkbootimg --kernel workspace/UEFI -o workspace/boot.img
 cd workspace/
