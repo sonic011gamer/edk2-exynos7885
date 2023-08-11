79416d3c92a9f0b1d1f2a72f7780fd8b3f456974
sonic011gamer <efimgirjeu@gmail.com>
2023-08-11 09:00:12 +0300

    Add CI build back.

--- ci-build.sh
+++ ci-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 sudo apt update
-sudo apt install -y build-essential uuid-dev iasl git nasm gcc-aarch64-linux-gnu bc
+sudo apt install -y build-essential uuid-dev iasl git nasm gcc-aarch64-linux-gnu bc mkbootimg
 curdir="$PWD"
 cd ..
-git clone https://github.com/tianocore/edk2.git --recursive
+git clone https://github.com/tianocore/edk2 --recursive -b edk2-stable202302
 git clone https://github.com/tianocore/edk2-platforms.git
 cd "$curdir"
 ./firstrun.sh
